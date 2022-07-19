b9c098f27e660eee232ce22348da0afb183787af
Aayush Niraula <aayush@accessworld.net>
2022-07-19 12:23:11 +0545

    For Loop

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,10 +1,26 @@
  <template>
     <div>
         <h1 v-if="show">This is from HomeComponent</h1>
-        <h1 v-else>This is from HomeComponent else block</h1>   
-        <!-- <h2>{{data}}</h2>  
-        <p>{{msg}}</p> -->
-        <button v-on:click="display">Click Here</button>      
+        <h1 v-else>Clicked button to toggle next block</h1>   
+        <h2>{{data}}</h2>  
+        <p>{{msg}}</p>
+        <button v-on:click="display">Click Here</button>
+        <br>
+        <h1>For Loop</h1>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Email</th>
+            </tr>
+            <tr v-for="user in users" :key="user.id">
+                 <th>{{user.id}}</th>
+                <th>{{user.name}}</th>
+                <th>{{user.age}}</th>
+                <th>{{user.email}}</th>
+            </tr>
+        </table>
     </div>
  </template>
   <!-- <script>
@@ -20,7 +36,7 @@
     }
  }
  </script> -->
- <script>
+ <!-- <script>
  export default {
     name: 'HomeComponent',
     props:{
@@ -32,7 +48,28 @@
         }
     },data()
     {
-        return {show:false}
+        return {show:true}
+    }
+ }
+ </script> -->
+ 
+ <!-- LOOP -->
+<script>
+ export default {
+    name: 'HomeComponent',
+    props:{
+        data: String,
+        msg: String,
+    },data()
+    {
+        return {
+            users:[
+                {id:1, name:'Peter', age:'23', email:'peter@email.com'},
+                {id:2, name:'Sam', age:'22', email:'sam@email.com'},
+                {id:3, name:'Tony', age:'23', email:'tony@email.com'},
+                {id:4, name:'Wanda', age:'25', email:'wanda@email.com'},
+            ]
+        }
     }
  }
  </script>
