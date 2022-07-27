393bfa385d6f02bd9594f1f87bbaaf95bb8f45b7
Aayush Niraula <aayush@accessworld.net>
2022-07-27 11:11:29 +0545

    Watchers in vue

--- src/components/AboutPageComponent.vue
+++ src/components/AboutPageComponent.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
         <h1>About Page</h1>
+        <h2>Watchers example</h2>
+        <h3>{{count}}</h3>
+        <button v-on:click="count=count+1">Increase</button>
+        <button v-on:click="count=count-1">Decrease</button>
     </div>
 </template>
 
 <script>
 export default {
     name:'AboutPageComponent',
+    data(){
+        return{
+            count:0
+        }
+    },watch:{
+        count(val,prev)
+        {
+           if(val>5 && val>prev){
+            this.count=0
+            alert("Stop counting");
+           }
+        }
+    }
 }
 </script>
